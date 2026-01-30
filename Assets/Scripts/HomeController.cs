2a6994ce71cb90a16b46c3554013c57e031ec73c
Hoàng Nguyễn <nqh130901@gmail.com>
2026-01-30 11:39:43 +0700

    feat: update characters

--- Assets/Scripts/HomeController.cs
+++ Assets/Scripts/HomeController.cs
@@ -1,18 +1,53 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class HomeController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    [Header("Loading")]
+    [SerializeField] private GameObject loadingPanel;
+    [SerializeField] private Slider loadingSlider;
 
+    [Header("Start")]
+    [SerializeField] private GameObject startButton;
+
+    [SerializeField] private float loadingDuration = 2f;
+
+    private void Start()
+    {
+        if (startButton != null)
+            startButton.SetActive(false);
+
+        if (loadingPanel != null)
+            loadingPanel.SetActive(true);
+
+        StartCoroutine(PlayLoadingThenShowStart());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator PlayLoadingThenShowStart()
     {
+        if (loadingSlider != null)
+            loadingSlider.value = 0f;
 
+        float elapsed = 0f;
+        while (elapsed < loadingDuration)
+        {
+            elapsed += Time.deltaTime;
+            float progress = Mathf.Clamp01(elapsed / loadingDuration);
+            if (loadingSlider != null)
+                loadingSlider.value = progress;
+            yield return null;
+        }
+
+        if (loadingSlider != null)
+            loadingSlider.value = 1f;
+
+        if (loadingPanel != null)
+            loadingPanel.SetActive(false);
+
+        if (startButton != null)
+            startButton.SetActive(true);
     }
 
     public void OnClickStart()
